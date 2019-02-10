d3cef6365c04eb20e7ee3981d3f451421206cd1f
Rahul Bhartari <engineerbhartarisingh@gmail.com>
2019-02-11 01:03:34 +0530

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 INC="../inc/"
-SRC="../src/*"
+SRC="../src/"
 LIB="-lm"
+CMODE="-c"
+COMPILER="gcc"
+DEF="MODE_CPP"
+
+if [ "x"$1 = "x"$CMODE ]
+then
+    SRC=$SRC'*.c'
+    DEF="MODE_C"
+else
+    SRC=$SRC'*.cpp'
+    COMPILER="g++"
+fi
 
 #build library with test code
-g++ -o testmtx -I $INC $SRC $LIB -O3
-echo "Build complete"
+$COMPILER -o testmtx -I $INC $SRC $LIB -O3 -D$DEF
+echo $SRC "Build complete"
